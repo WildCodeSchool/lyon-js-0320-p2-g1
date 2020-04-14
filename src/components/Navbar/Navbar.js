eb271b15c10f4ab1713efc5bfeb131369cd28460
Anaïs Huoch <anaisgagneur@gmail.com>
2020-04-14 18:32:08 +0200

    adding some elements to the router

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,49 +2,50 @@ import React from 'react';
 import './components/Navbar/Navbar.css';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import './src/images/logo.jpg';
 
 function Navbar () {
   return (
-  	<Router>
-  		<div>
-  			<div>
-  				<img src='' className='' alt=''/>
-  				<h1>Find your Cocktail</h1>
-  			</div>
+    <Router>
+      <div>
+			  <div>
+				  <img src= './src/images/logo.jpg' className='logo' alt='logo'/>
+				  <h1>Cocktail Finder</h1>
+			  </div>
   			
         <div className='NavbarContainer'>
-    		  <nav className='Navbar'>
-  				  <ul className='NavLinkContainer'>
-  					  <li className='NavLink'><Link to="/">Home</Link></li>
-  					  <li className='NavLink'><Link to="/">Cocktail Finder</Link></li>
-  					  <li className='NavLink'><Link to="/">All Cocktails</Link></li>
-  					  <li className='NavLink'><Link to="/">Guest Book</Link></li>
-  					  <li className='NavLink'><Link to="/">Quizz</Link></li>
-      			  </ul>
-    		  </nav>
+		      <nav className='Navbar'>
+				    <ul className='NavLinkContainer'>
+					    <li className='NavLink'><Link to='/'>Home</Link></li>
+					    <li className='NavLink'><Link to='/Find-your-cocktail'>Find your cocktail</Link></li>
+					    <li className='NavLink'><Link to='/All-Cocktails'>All Cocktails</Link></li>
+					    <li className='NavLink'><Link to='/Guest-Book'>Guest Book</Link></li>
+					    <li className='NavLink'><Link to='/Quizz'>Quizz</Link></li>
+			      </ul>
+		      </nav>
         </div>
 
-    		<Switch>
-          		<Route exact path="/"><Home /></Route>
-          		<Route path="/" component={} />
-          		<Route path="/" component={} />
-          		<Route path="/" component={} />
-          		<Route path="/.../:..." component={} />
-          	</Switch>
-        </div>
+        <Switch>
+		      <Route exact path='/'><Home /></Route>
+		      <Route path='/Find-your-cocktail' component={FindYourCocktail} />
+		      <Route path='/All-Cocktails' component={AllCocktails} />
+		      <Route path='/Guest-Book' component={GuestBook} />
+		      <Route path='/Quizz' component={Quizz} />
+        </Switch>
+      </div>
     </Router>
   );
 }
 
-function Home() {
+function Home () {
   return <h2>Home</h2>;
 }
 
 export default Navbar;
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Navbar />, rootElement);
 
-{/* // Si on utilise : <Route path="/.../:githubLogin" component={} /> afin de récupérer dans l'URL
+{ /* // Si on utilise : <Route path="/.../:githubLogin" component={} /> afin de récupérer dans l'URL
 
 // dans la page du composant correspondant (ex) :
 // import React from 'react';
@@ -61,4 +62,4 @@ ReactDOM.render(<App />, rootElement);
 //   );
 // }
 
-// export default UsersList; */}
+// export default UsersList; */ }
