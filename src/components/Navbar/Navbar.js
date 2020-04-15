a489aaea2de031ef0aa83c089f108dd44f34d70d
Anaïs Huoch <anaisgagneur@gmail.com>
2020-04-15 23:57:11 +0200

    styling with bootstrap

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,29 +4,30 @@ import FindYourCocktail from '../FindYourCocktail/FindYourCocktail';
 import AllCocktails from '../AllCocktails/AllCocktails';
 import GuestBook from '../GuestBook/GuestBook';
 import Quizz from '../Quizz/Quizz';
+import Logo from '../../images/logo.jpg';
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
 function Navbar () {
   return (
     <Router>
       <div>
-        <img src='../images/logo.jpg' className='logo' alt='logo'/> 
-			    <div className='websiteTitleContainer'>
-				    <h1>Cocktail Finder</h1>
-            <img src='../images/subTitleImg.png' className='subTitleImg' alt='subTitleImg'/> 
-			    </div>
-  			
-          <div className='NavbarContainer'>
-		        <nav className='Navbar'>
-				      <ul className='NavLinkContainer'>
-					      <li className='NavLink'><Link to='/'>Home</Link></li>
-					      <li className='NavLink'><Link to='/Find-your-cocktail'>Find your cocktail</Link></li>
-					      <li className='NavLink'><Link to='/All-Cocktails'>All Cocktails</Link></li>
-					      <li className='NavLink'><Link to='/Guest-Book'>Guest Book</Link></li>
-					      <li className='NavLink'><Link to='/Quizz'>Quizz</Link></li>
-			        </ul>
+		    <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
+          <a id='navbar-brand' href='#'><img src={Logo} id='logo' alt='logo'/> </a>
+            <h1 className='navbar-brand'>Cocktail Finder</h1>
+              {/* C'est le bouton burger (navbar-toggler) */}
+            <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarColor01' aria-controls='navbarColor01' aria-expanded='false' aria-label='Toggle navigation'>
+            <span class='navbar-toggler-icon'></span>
+            </button>
+            <div className='collapse navbar-collapse' id='navbarColor01'>
+				      <ul className='navbar-nav mr-auto '>
+					      <li className='nav-item active'><Link to='/' className='nav-link float-right'>Home</Link></li>
+					      <li className='nav-item'><Link to='/Find-your-cocktail' className='nav-link float-right'>Find your cocktail</Link></li>
+					      <li className='nav-item'><Link to='/All-Cocktails' className='nav-link float-right'>All Cocktails</Link></li>
+					      <li className='nav-item'><Link to='/Guest-Book' className='nav-link float-right'>Guest Book</Link></li>
+					      <li className='nav-item'><Link to='/Quizz' className='nav-link float-right'>Quizz</Link></li>
+			      </ul>
+              </div>
 		        </nav>
-          </div>
 
         <Switch>
 		      <Route exact path='/'><Home /></Route>
