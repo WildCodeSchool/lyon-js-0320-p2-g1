de22ecbef0a95b9c4be5c5110813acbdb33d4ba1
Anaïs Huoch <anaisgagneur@gmail.com>
2020-04-15 23:56:24 +0200

    styling with bootstrap

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar/Navbar.js';
+import 'bootswatch/dist/lux/bootstrap.min.css';
+
 
 function App () {
   return (
