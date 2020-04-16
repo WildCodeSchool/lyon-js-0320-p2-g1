a30a640ce9a15923369e3e531ee5e4d3057b1c64
Anaïs Huoch <anaisgagneur@gmail.com>
2020-04-16 18:19:30 +0200

    fixing className issue + fixing bg color problem

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Navbar.css';
-import FindYourCocktail from '../FindYourCocktail/FindYourCocktail';
+import FindYourCocktail from '../find-your-cocktail/FindYourCocktail';
 import AllCocktails from '../AllCocktails/AllCocktails';
 import GuestBook from '../GuestBook/GuestBook';
 import Quizz from '../Quizz/Quizz';
@@ -11,30 +11,30 @@ function Navbar () {
   return (
     <Router>
       <div>
-		    <nav className='navbar navbar-expand-lg navbar-dark bg-primary'>
-          <a id='navbar-brand' href='./components/Home/Home.js'><img src={Logo} id='logo' alt='logo'/> </a>
-            <h1 className='navbar-brand'>Cocktail Finder</h1>
-              {/* C'est le bouton burger (navbar-toggler) */}
-            <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarColor01' aria-controls='navbarColor01' aria-expanded='false' aria-label='Toggle navigation'>
-            <span className='navbar-toggler-icon'></span>
-            </button>
-            <div className='collapse navbar-collapse justify-content-end' id='navbarColor01'>
-				      <ul className='navbar-nav text-right'>
-					      <li className='nav-item active'><Link to='/Home.js' className='nav-link'>Home</Link></li>
-					      <li className='nav-item'><Link to='/Find-your-cocktail' className='nav-link'>Find your cocktail</Link></li>
-					      <li className='nav-item'><Link to='/All-Cocktails' className='nav-link'>All Cocktails</Link></li>
-					      <li className='nav-item'><Link to='/Guest-Book' className='nav-link'>Guest Book</Link></li>
-					      <li className='nav-item'><Link to='/Quizz' className='nav-link'>Quizz</Link></li>
-			      </ul>
-              </div>
-		        </nav>
+        <nav className='navbar navbar-expand-lg navbar-dark'>
+          <a id='navbar-brand' href='./components/Home/Home.js'><img src={Logo} id='logo' alt='logo' /> </a>
+          <h1 className='navbar-brand'>Cocktail Finder</h1>
+          {/* C'est le bouton burger (navbar-toggler) */}
+          <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarColor01' aria-controls='navbarColor01' aria-expanded='false' aria-label='Toggle navigation'>
+            <span className='navbar-toggler-icon' />
+          </button>
+          <div className='collapse navbar-collapse justify-content-end' id='navbarColor01'>
+            <ul className='navbar-nav text-right'>
+              <li className='nav-item active'><Link to='/Home.js' className='nav-link'>Home</Link></li>
+              <li className='nav-item'><Link to='/Find-your-cocktail' className='nav-link'>Find your cocktail</Link></li>
+              <li className='nav-item'><Link to='/All-Cocktails' className='nav-link'>All Cocktails</Link></li>
+              <li className='nav-item'><Link to='/Guest-Book' className='nav-link'>Guest Book</Link></li>
+              <li className='nav-item'><Link to='/Quizz' className='nav-link'>Quizz</Link></li>
+            </ul>
+          </div>
+        </nav>
 
         <Switch>
-		      <Route exact path='/Home.js'><Home /></Route>
-		      <Route exact path='/Find-your-cocktail' component={FindYourCocktail} />
-		      <Route exact path='/All-Cocktails' component={AllCocktails} />
-		      <Route exact path='/Guest-Book' component={GuestBook} />
-		      <Route exact path='/Quizz' component={Quizz} />
+          <Route exact path='/Home.js'><Home /></Route>
+          <Route exact path='/Find-your-cocktail' component={FindYourCocktail} />
+          <Route exact path='/All-Cocktails' component={AllCocktails} />
+          <Route exact path='/Guest-Book' component={GuestBook} />
+          <Route exact path='/Quizz' component={Quizz} />
         </Switch>
       </div>
     </Router>
@@ -46,4 +46,3 @@ function Home () {
 }
 
 export default Navbar;
-
