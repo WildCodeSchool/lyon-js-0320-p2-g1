110f03bfb7266687d8b14e6bce255c83e2535e76
Lancelot Trapet <lancelot.trapet@hotmail.fr>
2020-04-16 11:32:07 +0200

    footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/footer/Footer';
+
 
 function App() {
   return (
     <div className="App">
       <h1>Cocktail Finder</h1>
+      <Footer />
+
     </div>
   );
 }
---
+++ src/components/footer/Footer.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './Footer.css';
+
+class Footer extends React.Component {
+    render () {
+        return(
+            
+            
+            
+             <footer className='test'>
+            <section>
+                <article class="summer"><h2>Tes cocktail que tu à aimer</h2></article>
+                </section>
+                <p className='consomation'>la consomation d'allcol est à modérer</p>
+                <section class="button"> </section>
+                <button type="button" class="btn btn-secondary btn-lg btn-block">Contact</button>
+                
+                <p className='copyright'>copyright 'cocktail-finder groupe1'</p>
+                <p className='user'>by lens78</p>
+                
+            
+            </footer> 
+
+            
+
+            
+            
+        );
+        
+    }
+}
+
+export default Footer;
