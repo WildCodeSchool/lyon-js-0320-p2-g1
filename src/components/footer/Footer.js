7b96e19300a471eef2f8324851a44ce1814830a1
Lancelot Trapet <lancelot.trapet@hotmail.fr>
2020-04-17 10:56:29 +0200

    nouveau truci

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -6,26 +6,24 @@ class Footer extends React.Component {
         return(     
             
 <footer className='bloc_page'>
-    <div className='boissons'>
-                    <h1> commentaire</h1>
+    <div className='com'>
+                    <h1>commentaire</h1>
                     <p>tes derniers commentaires</p>
                     <p>cette boisson est top</p>
                     <p>le 14 avril2020</p>
     </div>
     <div className='contact'>
                     <h1>contact</h1>
-                    <p className='bouton_rouge'>nous contacter</p>
-                    <img className="reseau-sociau" src="https://miro.medium.com/max/621/1*XyvBPSfQebxwD652YQOtqw.png" alt="Nos réseaux sociaux" title="Nos réseaux sociaux"></img>
-                    <p></p>
+                    <button>nous contacter</button>
+                    <img src="https://miro.medium.com/max/621/1*XyvBPSfQebxwD652YQOtqw.png" alt="Nos réseaux sociaux" title="Nos réseaux sociaux"></img>
     </div>
 	<div className='copiright'>
                     <p>copiright by lens78inc2020</p>
                     <p>aplicationfind the drink</p>
                     <p>la consomation d'alcool</p>
                     <p>est a modérer</p>
-                    <ul>
-                        <li><a href="#"><strong>Nos Conditions <span class="color-point">...</span></strong></a></li>
-                    </ul>
+                    <p><strong>Nos Conditions ...</strong></p>
+                    
                     
     </div>
     
