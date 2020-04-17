5c9a479e065246e4377a477af8319c86564e883a
Lancelot Trapet <lancelot.trapet@hotmail.fr>
2020-04-17 11:31:36 +0200

    nouveau trucio

--- src/App.js
+++ src/App.js
@@ -6,7 +6,6 @@ import Footer from './components/footer/Footer';
 function App() {
   return (
     <div className="App">
-      <h1>Cocktail Finder</h1>
       <Footer />
 
     </div>
--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,22 +7,22 @@ class Footer extends React.Component {
             
 <footer className='bloc_page'>
     <div className='com'>
-                    <h1>commentaire</h1>
-                    <p>tes derniers commentaires</p>
-                    <p>cette boisson est top</p>
-                    <p>le 14 avril2020</p>
+                    <h1>GuestBook</h1>
+                    <p>The last comment :</p>
+                    <p>"This cocktail was Great"</p>
+                    <p>14 april 2020</p> {/*ajouter l'aplication date  */}
     </div>
     <div className='contact'>
-                    <h1>contact</h1>
-                    <button>nous contacter</button>
+                    <h1>Contact</h1>
+                    <button>Contact us</button>
                     <img src="https://miro.medium.com/max/621/1*XyvBPSfQebxwD652YQOtqw.png" alt="Nos réseaux sociaux" title="Nos réseaux sociaux"></img>
     </div>
 	<div className='copiright'>
-                    <p>copiright by lens78inc2020</p>
-                    <p>aplicationfind the drink</p>
-                    <p>la consomation d'alcool</p>
-                    <p>est a modérer</p>
-                    <p><strong>Nos Conditions ...</strong></p>
+                    <p>copyright by lens78inc2020</p>
+                    <p>Application Cocktail Finder</p>
+                    <p>"Excessive drinking is </p>
+                    <p>dangerous for the health"</p>
+                    <p><strong>Our conditions...</strong></p>
                     
                     
     </div>
