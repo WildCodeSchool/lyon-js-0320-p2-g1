3305dedeb9206d1e38c9fd86f992c174a4c201ca
Anaïs Huoch <anaisgagneur@gmail.com>
2020-04-20 13:44:43 +0200

    working on Guestbook page skeleton

--- src/components/guestbook/GuestBook.js
+++ src/components/guestbook/GuestBook.js
@@ -3,9 +3,30 @@ import './GuestBook.css';
 
 function GuestBook () {
   return (
-    <div>
-      <p>GuestBook page</p>
-    </div>
+    <main className='mainGuestbook'>
+      <h1>Guestbook</h1>
+      <p>If you need any informations or if you have some suggestions, feel free to leave a comment !</p>
+      <form className='GuestbookForm'>
+        <label for='commentArea'>Your comments</label>
+        <textarea id='message' name='user_message' placeholder='Type your message here'></textarea>
+        <input type="submit" value="Send" />
+      </form>
+      <div className='commentList'>
+        <h2>Last comments</h2>
+        <div className='comments'>
+          <h3>Message from [User Name]:</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam diam neque, accumsan in sapien eget, scelerisque congue nulla. Vestibulum diam diam, lacinia a magna ut, dapibus hendrerit augue.</p>
+        </div>
+        <div className='comments'>
+          <h3>Message from [User Name]:</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam diam neque, accumsan in sapien eget, scelerisque congue nulla. Vestibulum diam diam, lacinia a magna ut, dapibus hendrerit augue.</p>
+        </div>
+        <div className='comments'>
+          <h3>Message from [User Name]:</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam diam neque, accumsan in sapien eget, scelerisque congue nulla. Vestibulum diam diam, lacinia a magna ut, dapibus hendrerit augue.</p>
+        </div>
+      </div>
+    </main>
   );
 }
 
---
+++ src/components/home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render () {
+    return (
+      <main>
+      <h1>Home Page</h1>
+      <p>You are on our website home page</p>
+      </main>
+      );
+    }
+  }
+  
+  export default Home;
