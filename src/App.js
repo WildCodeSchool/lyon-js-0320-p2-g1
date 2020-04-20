ffdd3330e0cba4a022c8d82e61560a086d781b31
Adama SONKO <thioff1@gmail.com>
2020-04-20 16:21:37 +0200

    #25 corretion

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Home from './components/Home/Home.js';
-import Navbar from './components/Navbar/Navbar.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App () {
   return (
     <div className='App'>
-      <Navbar />
       <Home />
     </div>
   );
