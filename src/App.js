26a704540b67435e8015ee9f31cc79fa8499b279
abdel <kikoolaa39@gmail.com>
2020-04-21 16:40:45 +0200

    #12 error app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Navbar from './components/Navbar/Navbar.js';
-import 'bootswatch/dist/lux/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App () {
