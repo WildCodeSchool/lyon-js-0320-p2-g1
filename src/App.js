0a0a759290a00b719685d9efb3ea1aa468dd4a3e
Anaïs Huoch <anaisgagneur@gmail.com>
2020-04-21 18:09:12 +0200

    styling page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './components/navbar/Navbar.js';
 import Main from './components/main/Main.js';
 import { BrowserRouter as Router } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.css';
 
 function App () {
   return (
