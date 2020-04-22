0547026a01c0b1ba63d0ba48fb04cc9988082466
Pierre Genthon <pierre.genthon@wildcodeschool.fr>
2020-04-23 00:04:14 +0200

    remove App.css import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Navbar from './components/navbar/Navbar.js';
 import Main from './components/main/Main.js';
 import { BrowserRouter as Router } from 'react-router-dom';
