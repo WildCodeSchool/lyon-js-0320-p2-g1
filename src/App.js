2bd256ec2b9f085ecc46026caa1908641e6ca783
Kosi <hugo.pioda@gmail.com>
2020-04-28 17:00:42 +0200

    fix path

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import Navbar from './components/navbar/Navbar.js';
-import Main from './components/main/Main.js';
+import Navbar from './components/navbar/Navbar';
+import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
 import { BrowserRouter as Router } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
-import Footer from './components/footer/Footer';
 import 'bootswatch/dist/lux/bootstrap.min.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
