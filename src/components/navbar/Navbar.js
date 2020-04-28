3a7727821fede9048c998f43afc3d4292af3789d
Kosi <hugo.pioda@gmail.com>
2020-04-28 17:13:10 +0200

    Fix merge conflict nav

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,45 +1,40 @@
 import React from 'react';
 import './Navbar.css';
-import Home from '../home/Home';
-import FindYourCocktail from '../find-your-cocktail/FindYourCocktail';
-import AllCocktails from '../all-cocktails/AllCocktails';
-import GuestBook from '../guestbook/GuestBook';
-import Quizz from '../quizz/Quizz';
 import Logo from '../../images/logo.jpg';
-import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
-function Navbar () {
+export default function NavBar () {
   return (
-    <Router>
-      <div>
-        <nav className='navbar navbar-expand-lg navbar-dark'>
-          <a id='navbar-brand' href='./components/Home/Home.js'><img src={Logo} id='logo' alt='logo' /> </a>
-          <h1 className='navbar-brand'>Cocktail Finder</h1>
-          {/* C'est le bouton burger (navbar-toggler) */}
-          <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarColor01' aria-controls='navbarColor01' aria-expanded='false' aria-label='Toggle navigation'>
-            <span className='navbar-toggler-icon' />
-          </button>
-          <div className='collapse navbar-collapse justify-content-end' id='navbarColor01'>
-            <ul className='navbar-nav text-right'>
-              <li className='nav-item active'><Link to='/' className='nav-link'>Home</Link></li>
-              <li className='nav-item'><Link to='/Find-your-cocktail' className='nav-link'>Find your cocktail</Link></li>
-              <li className='nav-item'><Link to='/All-Cocktails' className='nav-link'>All Cocktails</Link></li>
-              <li className='nav-item'><Link to='/Guest-Book' className='nav-link'>Guest Book</Link></li>
-              <li className='nav-item'><Link to='/Quizz' className='nav-link'>Quizz</Link></li>
-            </ul>
-          </div>
-        </nav>
+    <div>
+      <nav className='navbar navbar-expand-lg navbar-dark'>
+        <a id='navbar-brand' href='./components/Home/Home.js'><img src={Logo} id='logo' alt='logo' /> </a>
+        <h1 className='navbar-brand'>Cocktail Finder</h1>
+        {/* C'est le bouton burger (navbar-toggler) */}
+        <button className='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarColor01' aria-controls='navbarColor01' aria-expanded='false' aria-label='Toggle navigation'>
+          <span className='navbar-toggler-icon' />
+        </button>
+        <div className='collapse navbar-collapse justify-content-end' id='navbarColor01'>
 
-        <Switch>
-          <Route exact path='/'><Home /></Route>
-          <Route path='/Find-your-cocktail' component={FindYourCocktail} />
-          <Route path='/All-Cocktails' component={AllCocktails} />
-          <Route path='/Guest-Book' component={GuestBook} />
-          <Route path='/Quizz' component={Quizz} />
-        </Switch>
-      </div>
-    </Router>
+          <ul className='navbar-nav text-right'>
+            <li className='nav-item active'>
+              <NavLink exact to='/' className='nav-link'>Home</NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink exact to='/find-your-cocktail' className='nav-link'>Find your cocktail</NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink exact to='/all-cocktails' className='nav-link'>All Cocktails</NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink exact to='/guestbook' className='nav-link'>Guest Book</NavLink>
+            </li>
+            <li className='nav-item'>
+              <NavLink exact to='/quizz' className='nav-link'>Quizz</NavLink>
+            </li>
+          </ul>
+
+        </div>
+      </nav>
+    </div>
   );
 }
-
-export default Navbar;
