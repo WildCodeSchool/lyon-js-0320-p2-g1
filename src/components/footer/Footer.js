6d7baf73373fe7dbe1525253cae64c38789364c9
Anaïs Huoch <anaisgagneur@gmail.com>
2020-05-11 17:53:10 +0200

    style refactoring

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -5,24 +5,11 @@ class Footer extends React.Component {
   render () {
     return (
       <footer className='bloc_page'>
-        <div className='com'>
-          <img src='https://pngimage.net/wp-content/uploads/2018/06/logo-cocktail-png-6.png' alt='logo' title='logo' />
-          <h3>GuestBook</h3>
-          <p>The last comment :</p>
-          <p>"This cocktail was Great"</p>
-          <p>14 april 2020</p>
-        </div>
         <div className='contact'>
-          <h3>Contact</h3>
           <a href='mailto:cocktail.finder@outlook.fr'>Contact us</a>
-          <img src='https://miro.medium.com/max/621/1*XyvBPSfQebxwD652YQOtqw.png' alt='Nos réseaux sociaux' title='Nos réseaux sociaux' />
         </div>
-        <div className='copiright'>
-          <p>copyright by lens78inc2020</p>
-          <p>Application Cocktail Finder</p>
-          <p className='phrases'>"Excessive drinking is </p>
-          <p className='phrases'>dangerous for the health"</p>
-          <p><strong>Our conditions...</strong></p>
+        <div className='warning-container'>
+          <p className='warning'>Excessive drinking is dangerous for the health</p>
         </div>
       </footer>
     );
